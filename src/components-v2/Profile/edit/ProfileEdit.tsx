3ab032e96da06c8532a6729851578eb7b78aebea
immhrmwn <immd.flores@gmail.com>
2021-11-30 19:16:44 +0800

    MYR-1097: added warn prompt when leaving page

--- src/components-v2/Profile/edit/ProfileEdit.tsx
+++ src/components-v2/Profile/edit/ProfileEdit.tsx
@@ -65,6 +65,28 @@ export const ProfileEditComponent: React.FC<Props> = props => {
     usernameValidation();
   }, [newUser, username]);
 
+  useEffect(() => {
+    if (isEdited) {
+      window.addEventListener('beforeunload', function (e) {
+        // Cancel the event
+        e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+        // Chrome requires returnValue to be set
+        e.returnValue = '';
+      });
+    } else {
+      window.removeEventListener('beforeunload', function (e) {
+        e.preventDefault();
+        e.returnValue = '';
+      });
+    }
+    return () => {
+      window.removeEventListener('beforeunload', function (e) {
+        e.preventDefault();
+        e.returnValue = '';
+      });
+    };
+  }, [isEdited]);
+
   const handleChangeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
     handleUsernameAvailable(event.target.value);
