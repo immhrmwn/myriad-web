3826b0a9724d691bd26a41b318e6297a97263116
immhrmwn <immd.flores@gmail.com>
2022-05-12 16:00:32 +0700

    MYR-2011: created new page serve list

---
+++ pages/server-list.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import {getSession} from 'next-auth/client';
+
+import {ServerListComponent} from 'src/components/ServerList/ServerList.container';
+import {generateAnonymousUser} from 'src/helpers/auth';
+import {healthcheck} from 'src/lib/api/healthcheck';
+import {setAnonymous} from 'src/reducers/user/actions';
+import {wrapper} from 'src/store';
+import {ThunkDispatchAction} from 'src/types/thunk';
+
+const ServerList: React.FC = () => {
+  return <ServerListComponent />;
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(store => async context => {
+  const {req} = context;
+  const {headers} = req;
+
+  const dispatch = store.dispatch as ThunkDispatchAction;
+
+  if (typeof window === 'undefined' && headers['user-agent']) {
+    const UAParser = eval('require("ua-parser-js")');
+    const parser = new UAParser();
+    const device = parser.setUA(headers['user-agent']).getDevice();
+
+    if (device.type === 'mobile') {
+      return {
+        redirect: {
+          destination: '/mobile',
+          permanent: false,
+          headers,
+        },
+      };
+    }
+  }
+
+  const available = await healthcheck();
+
+  if (!available) {
+    return {
+      redirect: {
+        destination: '/maintenance',
+        permanent: false,
+      },
+    };
+  }
+
+  const session = await getSession(context);
+
+  const anonymous = session ? false : true;
+  const userId = session?.user.address as string;
+
+  if (anonymous || !userId) {
+    const username = generateAnonymousUser();
+
+    await dispatch(setAnonymous(username));
+  }
+
+  return {
+    props: {
+      session: session ?? null,
+    },
+  };
+});
+
+export default ServerList;
